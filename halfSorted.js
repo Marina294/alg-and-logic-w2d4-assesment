04c4e80b4ce9c10a4a3ab7384b628ce04d03610b
Marina294 <69162947+Marina294@users.noreply.github.com>
2020-09-25 08:29:55 -0700

    first

--- halfSorted.js
+++ halfSorted.js
@@ -10,8 +10,37 @@ let arr2 = [56, 98, 65, 3, 4, 58, 68, 90, 12, 34, 45]
 
 
 const halfSorted = function(arr) {
-
+    if (arr.length < 2) {
+        return arr
+      }
+    
+      const middle = Math.floor(arr.length / 2);
+      const leftSide = arr.slice(0, middle);
+      const rightSide = arr.slice(middle, arr.length);
+      return merge(mergeSort(leftSide), mergert(rightSide))
+    
+    };
+    
+    const merge = (left, right) => {
+      const result = [];
+    
+      while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+          result.push(left.shift());
+        } else {
+          result.push(right.shift);
+        }
+      }
+    
+      while(left.length) result.push(left.shift());
+    
+      while(right.length) result.push(right.shift());
+    
+      return result;
 };
+  
+    
+
 
 
 console.log(halfSorted(arr1));
--- insertionSort.js
+++ insertionSort.js
@@ -7,10 +7,59 @@ let arr1 = [4, 78, 54, 34, 2, 8, 45, 77, 99, 23, 567, 6, 7];
 let arr2 = [56, 98, 65, 3, 4, 58, 68, 90, 12, 34, 45];
 
 
-const insertionSort = function(arr) {
 
+const insertionSort = function(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        let j = i - 1;
+        // if (x[i -1] > current) {
+        // let j = i;
+        // for (let j = 1; j < arr.length; j++) {
+            while ( j >= 0 && arr[j] > current) {
+             arr[j + 1] = arr[j];
+             j = j - 1;
+
+            // }
+         }
+
+            arr[j + 1] = current;
+            // i = i + 1;
+        // }
+    }
+
+ return arr;
 };
 
 console.log(insertionSort(arr1));
 
 console.log(insertionSort(arr2));
+
+
+
+
+// for (i = 0; i < arr.length; i++) {
+//     let current = arr[pointer];
+//     for (j = 0; j < arr.length; j++) {
+//         let position = pointer;
+//         let i = 0;
+//         while (position > 0 && arr[position - 1] > current) {
+//           arr[position] = arr[position - 1];
+//           position = position - 1;
+//         }
+//     arr[position] = current;
+// }
+// return pointer
+// }
+
+
+// let i = pointer;
+// for (i = 0; i < arr.length; i++) {
+//     let current = arr[pointer];
+//     let position = pointer;
+//     while (position > 0 && arr[position - 1] > current) {
+//         arr[position] = arr[position - 1];
+//         let position = position - 1;
+//     }
+//     arr[position] = current;
+// }
+// return pointer
